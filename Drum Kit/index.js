8a177d3fb1d0c99a0ea217397db61bdeecd01706
BrahmeshwarThakur <103745828+BrahmeshwarThakur@users.noreply.github.com>
2023-05-14 23:42:21 +0530

    Add files via upload

---
+++ Drum Kit/index.js
@@ -0,0 +1,63 @@
+var noOfDrumbuttons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < noOfDrumbuttons; i++) {
+
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    var buttomInnerHTML = this.innerHTML;
+    makeSound(buttomInnerHTML);
+    buttonAnimation(buttomInnerHTML);
+
+
+  });
+
+}
+document.addEventListener("keydown", function(event){
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
+function makeSound(key){
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    case "j":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    case "k":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+    case "l":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+
+      break;
+    default:
+      console.log(buttomInnerHTML);
+
+  }
+}
+
+function buttonAnimation(currentKey){
+var activeButton=document.querySelector("." + currentKey)
+activeButton.classList.add("pressed");
+
+setTimeout(function() {
+  activeButton.classList.remove("pressed");
+},100);
+
+}
